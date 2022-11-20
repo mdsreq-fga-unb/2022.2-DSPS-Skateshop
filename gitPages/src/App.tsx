78f30a1404a5b45b600b55fe94647e18436e49c7
Lucas <sales_l@hotmail.com>
2022-11-20 14:07:54 -0300

    using vite instead of create-react-app

---
+++ gitPages/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1>teste vite</h1>;
+}
+
+export default App;
