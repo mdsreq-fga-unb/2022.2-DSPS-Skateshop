0131f976458a7ee051c1c7fa9e9a4bff926626e4
dionvitor <dionvictor11@gmail.com>
2023-01-06 19:09:27 -0300

    feature: adicionado página home, sobre nós e contate-nos, além de adicionar modelos de categorias e produtos para o site.

---
+++ api/pages/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+
+class ContactPageView(TemplateView):
+    template_name = 'contact.html'
