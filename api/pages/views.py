12235c4cf3444bbcf15a940c00aaed0510a9f471
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2023-01-10 12:15:40 -0300

    adicionar contatos à página home
    e adicionar mais campos além do shape e dos trucks. Os campos estão na ordem de montagem: shape, lixa, truck e assim por diante.

--- api/pages/views.py
+++ api/pages/views.py
@@ -6,6 +6,11 @@ from company.models import Company
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context["company"] = Company.objects.all().first()
+        return context
+
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
