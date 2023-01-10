6c28254e844881472d7b9f60250418c4a10bdf92
dionvitor <dionvictor11@gmail.com>
2023-01-10 10:59:15 -0300

    feature: adicionado dados da empresa no site de forma dinámica.

--- api/pages/views.py
+++ api/pages/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from company.models import Company
+
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -8,6 +10,16 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = 'about.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(AboutPageView, self).get_context_data(**kwargs)
+        context["company"] = Company.objects.all().first()
+        return context
+
 
 class ContactPageView(TemplateView):
     template_name = 'contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactPageView, self).get_context_data(**kwargs)
+        context["company"] = Company.objects.all().first()
+        return context
